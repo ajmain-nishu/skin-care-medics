711a88a32fd51f6ea6d812e3941f72e86d6add0c
Ajmain Nishu <ajmainnishu@gmail.com>
2021-11-08 13:13:23 +0600

    test: bug fix in redirect

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCaLyrCI1V6Sh4nFL12SGWoYxO8geFmulI",
-    authDomain: "skin-care-medics.firebaseapp.com",
-    projectId: "skin-care-medics",
-    storageBucket: "skin-care-medics.appspot.com",
-    messagingSenderId: "672592295618",
-    appId: "1:672592295618:web:2d7da3272dfd7254b2ed5b"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation, useHistory } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import './Login.css';
@@ -7,86 +7,128 @@ import './Login.css';
 
 // navbar login section
 const Login = () => {
-    const { signInUsingGoogle, loginError, handleEmail, handleLogin, handlePassword } = useAuth();
-    const location = useLocation()
-    const history = useHistory()
-    const redirect_uri = location.state?.from || '/home'
+  const { signInWithGoogle, setUser, loginWithEmailAndPassword, setIsLoading } = useAuth();
 
-    // google sign in arrow functio button
-    const handleGoogleLogin = () => {
-        signInUsingGoogle()
-        .then(result => {
-            history.push(redirect_uri)
-        })
-    }
+  const history = useHistory()
+  const location = useLocation()
+
+  const url = location.state?.from || "/home"
 
 
-    return (
-        <div className="login__bg">
-            <div className="container">
-                <div className="row">
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
 
-                    {/* login form */}
-                    <div className="col-md-4 col-12 pt-5 mt-5">
-                        <div className="bg-light shadow rounded px-4 py-2 text-center form-color">
-                            <form
-                                onSubmit={handleLogin}>
-                                <h3 className="pt-3">Login Today</h3>
+  // email
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  }
 
-                                {/* email part */}
-                                <div className="my-3">
-                                    <input
-                                        onBlur={handleEmail}
-                                        className="form-control form-control-lg"
-                                        type="email"
-                                        name=""
-                                        id="login_email"
-                                        placeholder="Email"
-                                    />
-                                </div>
+  // password
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  }
 
-                                {/* password part */}
-                                <div className="my-3">
-                                    <input
-                                        onBlur={handlePassword}
-                                        className="form-control form-control-lg"
-                                        type="password"
-                                        name=""
-                                        id="login_password"
-                                        placeholder="Password"
-                                    />
-                                </div>
+  // login email
+  const handleLoginWithEmailAndPassword = (e) => {
+    e.preventDefault();
 
-                                {/* error part */}
-                                <div className="text-danger">{loginError}</div>
+    loginWithEmailAndPassword(email, password)
+      .then((res) => {
+        setIsLoading(true)
+        setUser(res.user);
+        history.push(url)
+      })
+      .catch((error) => {
+        setError('Please correct email or password')
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
 
-                                {/* login button */}
-                                <div className="my-3">
-                                    <input className="btn btn-outline-secondary btn-lg" type="submit" value="Log In" />
-                                </div>
-                            </form>
+  //login google
+  const handleGoogleLogin = () => {
+    signInWithGoogle()
+      .then((res) => {
+        setIsLoading(true)
+        setUser(res.user)
+        history.push(url)
+      }
+      )
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setIsLoading(false)
+      })
+  };
 
 
-                            <p className="pb-1"></p>
-                            <hr />
+  return (
+    <div className="login__bg">
+      <div className="container">
+        <div className="row">
 
-                            {/* google sign in */}
-                            <div className="mb-3 mt-4">
-                                <button className="btn btn-outline-secondary btn-md" onClick={handleGoogleLogin}>Sign In With Google</button>
-                            </div>
+          {/* login form */}
+          <div className="col-md-4 col-12 pt-5 mt-5">
+            <div className="bg-light shadow rounded px-4 py-2 text-center form-color">
+              <form
+                onSubmit={handleLoginWithEmailAndPassword}>
+                <h3 className="pt-3">Login Today</h3>
 
-                            {/* new register link */}
-                            <Link to="/register"><button className="btn btn-secondary btn-md mb-3 mt-1">Create New Account</button></Link>
-                        </div>
-
-                    </div>
-
-                    {/* gap section */}
-                    <div className="col-md-8 col-12"></div>
+                {/* email part */}
+                <div className="my-3">
+                  <input
+                    onBlur={handleEmail}
+                    className="form-control form-control-lg"
+                    type="email"
+                    name=""
+                    id="login_email"
+                    placeholder="Email"
+                  />
                 </div>
+
+                {/* password part */}
+                <div className="my-3">
+                  <input
+                    onBlur={handlePassword}
+                    className="form-control form-control-lg"
+                    type="password"
+                    name=""
+                    id="login_password"
+                    placeholder="Password"
+                  />
+                </div>
+
+                {/* error part */}
+                <div className="text-danger">{error}</div>
+
+                {/* login button */}
+                <div className="my-3">
+                  <input className="btn btn-outline-secondary btn-lg" type="submit" value="Log In" />
+                </div>
+              </form>
+
+
+              <p className="pb-1"></p>
+              <hr />
+
+              {/* google sign in */}
+              <div className="mb-3 mt-4">
+                <button className="btn btn-outline-secondary btn-md" onClick={handleGoogleLogin}>Sign In With Google</button>
+              </div>
+
+              {/* new register link */}
+              <Link to="/register"><button className="btn btn-secondary btn-md mb-3 mt-1">Create New Account</button></Link>
             </div>
+
+          </div>
+
+          {/* gap section */}
+          <div className="col-md-8 col-12"></div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 
